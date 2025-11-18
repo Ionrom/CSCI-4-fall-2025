3438c34572f542c873388ba95703837364af5ef8
Ian Stuart <istuart001@student.butte.edu>
2025-11-18 17:24:05 +0000

    h

---
+++ Untitled-2.py
@@ -0,0 +1,17 @@
+
+# conditional choice
+
+
+def f(x, y,z):
+    if (z==0):
+        print("sum", x+y)
+    elif (z==1):
+        print("difference", abs (x-y))
+    else:
+        print("z=/= {0,1}")
+
+f(1,2,0) # guess: sum 3
+
+f(1,2,1) # guess: difference 1
+
+f(1,2,5) # guess: the string in the else
